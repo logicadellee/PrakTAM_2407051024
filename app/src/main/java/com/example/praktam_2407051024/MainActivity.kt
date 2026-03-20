9da0ae93215e2716c2f72f2293e775b7a6e9ad53
logicadellee <adelleee1@gmail.com>
2026-03-20 08:03:56 +0700

    Tugas LKP 4

--- app/src/main/java/com/example/praktam_2407051024/MainActivity.kt
+++ app/src/main/java/com/example/praktam_2407051024/MainActivity.kt
@@ -4,20 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material3.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,7 +32,7 @@ import com.example.praktam_2407051024.ui.theme.PrakTAM_2407051024Theme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        enableEdgeToEdge()
+
         setContent {
             PrakTAM_2407051024Theme {
                 Surface(modifier = Modifier.fillMaxSize()) {
@@ -45,10 +48,10 @@ fun ActivityList() {
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
-            .padding(24.dp),
-        verticalArrangement = Arrangement.spacedBy(24.dp)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        items(ActivitySource.dummyActivity) { activity: ActivityItem ->
+        items(ActivitySource.dummyActivity) { activity ->
             ActivityCard(activity)
         }
     }
@@ -57,20 +60,52 @@ fun ActivityList() {
 @Composable
 fun ActivityCard(activity: ActivityItem) {
 
+    var isFavorite by remember { mutableStateOf(false) }
+
     Card(
         modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(16.dp),
         elevation = CardDefaults.cardElevation(6.dp)
     ) {
 
         Column {
 
-            Image(
-                painter = painterResource(id = activity.imageRes),
-                contentDescription = activity.nama,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(180.dp)
-            )
+            Box {
+
+                Image(
+                    painter = painterResource(id = activity.imageRes),
+                    contentDescription = activity.nama,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp),
+                    contentScale = ContentScale.Crop
+                )
+
+                IconButton(
+                    onClick = { isFavorite = !isFavorite },
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(10.dp)
+                        .background(
+                            Color.Black.copy(alpha = 0.3f),
+                            shape = CircleShape
+                        )
+                ) {
+                    Icon(
+                        imageVector = if (isFavorite)
+                            Icons.Filled.Favorite
+                        else
+                            Icons.Outlined.FavoriteBorder,
+
+                        contentDescription = "Favorite",
+
+                        tint = if (isFavorite)
+                            Color.Red
+                        else
+                            Color.White
+                    )
+                }
+            }
 
             Column(
                 modifier = Modifier.padding(16.dp)
@@ -100,13 +135,18 @@ fun ActivityCard(activity: ActivityItem) {
 
                 Button(
                     onClick = { },
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(45.dp),
+                    shape = RoundedCornerShape(50),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFF8FA8FF)
+                    )
                 ) {
                     Text("Tandai Selesai")
                 }
 
             }
-
         }
     }
 }
